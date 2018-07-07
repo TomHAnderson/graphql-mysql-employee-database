d19180b496c4537b869af661cb7d83b93da8b47c
Tom H Anderson <tom.h.anderson@gmail.com>
2018-07-07 08:47:47 -0600

    Use php 5.7; fix perm names in auth listener

--- module/Application/src/Authorization/AuthorizationListener.php
+++ module/Application/src/Authorization/AuthorizationListener.php
@@ -15,9 +15,9 @@ final class AuthorizationListener
   #      $authorization->deny();
 
         $authorization->addResource('Application\Controller\IndexController::index');
-        $authorization->allow(RoleFixture::$GUEST, 'Application\Controller\IndexController::index');
+        $authorization->allow(RoleFixture::GUEST, 'Application\Controller\IndexController::index');
 
         $authorization->addResource('ZF\OAuth2\Controller\Auth::authorize');
-        $authorization->allow(RoleFixture::$USER, 'ZF\OAuth2\Controller\Auth::authorize');
+        $authorization->allow(RoleFixture::USER, 'ZF\OAuth2\Controller\Auth::authorize');
     }
 }
